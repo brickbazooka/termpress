0c43d56277bcde88f004877b84b6ecd3841ed479
Brick <brick.berserker@gmail.com>
2025-01-07 23:14:57 +0000

    Modify main.sh, to start serving (public) the generated content

--- src/main.py
+++ src/main.py
@@ -4,9 +4,14 @@ import shutil
 from copystatic import copy_files_recursive
 from generate import generate_page
 
+import os
+import shutil
+
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -17,7 +22,12 @@ def main():
 	print("Copying static files to public directory...")
 	copy_files_recursive(dir_path_static, dir_path_public)
 
-	generate_page("./content/index.md", "./template.html", "./public/index.html")
+	print("Generating page...")
+	generate_page(
+		os.path.join(dir_path_content, "index.md"),
+		template_path,
+		os.path.join(dir_path_public, "index.html"),
+	)
 
 
 main()

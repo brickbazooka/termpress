03116f3941736dea2141c67dc74ef724393dd555
Brick <brick.berserker@gmail.com>
2025-01-08 00:32:29 +0000

    Add generate#generate_pages_recursive to handle all the markdown files in the content dir.

--- src/generate.py
+++ src/generate.py
@@ -16,7 +16,7 @@ def generate_page(from_path, template_path, dest_path):
 	6. Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML and title you generated.
 	7. Write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist.
 	"""
-	print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+	print(f"* Generating page ({from_path} → {dest_path})...")
 
 	with open(template_path) as template_file:
 		template_content = template_file.read()
@@ -37,3 +37,20 @@ def generate_page(from_path, template_path, dest_path):
 
 	with open(dest_path, 'w') as dest:
 		dest.write(final_html_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+	"""
+	1. Crawl every entry in the content directory
+	2. For each markdown file found, generate a new .html file using the same template.html.
+	The generated pages should be written to the public directory in the same directory structure.
+	"""
+	with os.scandir(dir_path_content) as it:
+		for entry in it:
+			src_path = os.path.join(dir_path_content, entry.name)
+			dest_path = os.path.join(dest_dir_path, entry.name)
+
+			if entry.is_file() and entry.name.endswith(('.md', '.MD')):
+				dest_path = os.path.join(dest_dir_path, entry.name.replace('.md', '.html').replace('.MD', '.html'))
+				generate_page(src_path, template_path, dest_path)
+			else:
+				generate_pages_recursive(src_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-from generate import generate_page
+from generate import generate_pages_recursive
 
 import os
 import shutil
@@ -15,19 +15,22 @@ template_path = "./template.html"
 
 
 def main():
-	print("Deleting public directory...")
+	print(f"Deleting the public directory ({dir_path_public})...")
 	if os.path.exists(dir_path_public):
 		shutil.rmtree(dir_path_public)
+	print("\n")
 
-	print("Copying static files to public directory...")
+	print(f"Copying static files to public directory...")
 	copy_files_recursive(dir_path_static, dir_path_public)
+	print("\n")
 
-	print("Generating page...")
-	generate_page(
-		os.path.join(dir_path_content, "index.md"),
+	print(f"Generating pages (using {template_path})...")
+	generate_pages_recursive(
+		os.path.join(dir_path_content),
 		template_path,
-		os.path.join(dir_path_public, "index.html"),
+		os.path.join(dir_path_public),
 	)
+	print("\n")
 
 
 main()
